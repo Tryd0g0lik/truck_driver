3c970d440293b833a88bf1aaf3b1893f10bc7713
Denis <work80@mail.ru>
2025-08-12 17:29:06 +0700

    [master]:'person/tasks/*.py' Tasks - for cashe of user after registrate - was added

---
+++ person/tasks/task_cache_hew_user.py
@@ -0,0 +1,101 @@
+import json
+import logging
+import os
+from typing import Union
+from celery import shared_task
+from celery.utils.log import get_task_logger
+
+from redis import Redis, TimeoutError
+
+from dotenv_ import DB_TO_RADIS_CACHE_USERS, DB_TO_RADIS_PORT, DB_TO_RADIS_HOST
+from logs import configure_logging
+from person.interfaces import TypeUser
+from person.models import Users
+from person.serializers import CacheUsersSerializer
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+
+log = logging.getLogger(__name__)
+configure_logging(logging.INFO)
+
+logger = get_task_logger(__name__)
+
+
+@shared_task(
+    name=__name__,
+    bind=True,
+    ignore_result=True,
+    autoretry_for=(TimeoutError,),
+    retry_backoff=False,
+)
+def task_postman_for_user_id(self, user_id_list: list[int]) -> Union[TypeUser, dict]:
+    """
+    retry_backoff - https://docs.celeryq.dev/en/stable/userguide/tasks.html#Task.autoretry_for
+    ignore_result - If False, it means we don't have a response. Or conversely if we have a True.
+    https://docs.celeryq.dev/en/stable/userguide/tasks.html#Task.ignore_result
+
+    This Task is a POSTMAN. Here only transmits the user id. It happens from the entry point to the handler function
+    :param self:
+    :param user_id_list: Index from new user, we receive in list format.
+    :return:
+    """
+    if len(user_id_list) == 0:
+        log.error(ValueError("[%s]: No users found" % __name__))
+        return {}
+    return person_to_redis(user_id_list)
+
+
+def person_to_redis(user_id_list: list[int]) -> Union[TypeUser, dict]:
+    """
+    Here, we will be caching of new user after registration/ From entrypoint,we get an id.
+    After that, we find the user by id and send it to the cache.
+    :param self:
+    :param user_id_list: Index from new user, we receive in list format.
+    :return:
+    """
+    log.info("START CACHE: %s" % __name__)
+    client = Redis(
+        host=f"{DB_TO_RADIS_HOST}",
+        port=f"{DB_TO_RADIS_PORT}",
+        db=DB_TO_RADIS_CACHE_USERS,
+    )
+
+    try:
+        if not client.ping():
+            log.error(ConnectionError("Redis connection failed"))
+            return {}
+        log.info("Client is ping")
+        # Basis db
+        person_list = Users.objects.filter(id=user_id_list.__getitem__(0))
+        if not person_list.exists():
+            log.error(
+                ValueError(
+                    "[%s]: No users found in Users's db. Length from 'person_list' is %s "
+                    % (__name__, str(len(person_list)))
+                )
+            )
+            return {}
+        user_serializer = CacheUsersSerializer(person_list.__getitem__(0))
+        user_dict: TypeUser = user_serializer.data.copy()
+        log.info("Received user ID: %s" % user_dict.__getitem__("id"))
+        # Redis's cache
+        # Now will be saving on the 27 hours.
+        # 'task_user_from_cache' task wil be to upgrade postgres at ~ am 01:00
+        # Timetable look the 'project.celery.app.base.Celery.conf'
+        client.set(
+            f"user:{str(user_dict.__getitem__("id"))}:person",
+            json.dumps(user_dict),
+            97200,
+        )
+        client.close()
+        log.info(
+            "User with %s ID was saved in Redis. The End"
+            % str(user_dict.__getitem__("id"))
+        )
+        return user_dict
+    except Exception as error:
+        log.error("[%s]: ERROR => %s" % (__name__, error.args.__getitem__(0)))
+        log.info(
+            ValueError("[%s]: ERROR => %s" % (__name__, error.args.__getitem__(0)))
+        )
+        return {}
---
+++ person/tasks/task_user_is_authenticate.py
@@ -0,0 +1,99 @@
+import asyncio
+import json
+import logging
+from datetime import datetime
+from json import JSONDecodeError
+
+from redis.asyncio.client import Redis
+from redis.exceptions import ConnectionError, BusyLoadingError
+from redis.backoff import ExponentialBackoff
+from redis.retry import Retry
+from celery import shared_task
+
+from logs import configure_logging
+from person.interfaces import TypeUser
+from person.views_api.redis_person import RedisOfPerson
+
+
+from dotenv_ import DB_TO_RADIS_CACHE_USERS, DB_TO_RADIS_PORT, DB_TO_RADIS_HOST
+
+log = logging.getLogger(__name__)
+configure_logging(logging.INFO)
+
+
+@shared_task(
+    name=__name__,
+    bind=True,
+    ignore_result=True,
+    autoretry_for=(TimeoutError,),
+    retry_backoff=False,
+)
+def task_user_authenticate(self, user_id: int) -> dict | bool:
+    log.info("START CACHE: %s" % __name__)
+
+    """ "
+    Here, we get a dict from non-relational(Redis 1) DB and updating the properties (then saved to the Redis 1).
+    If all 'OK' we return True or empty dict (when wea have an Error), to the outside.
+    """
+    try:
+        asyncio.get_event_loop().run_until_complete(
+            async_task_user_authenticate(user_id)
+        )
+    except (ConnectionError, Exception) as error:
+        log.error(ValueError("%s: ERROR => %s" % (__name__, error.args.__getitem__(0))))
+        return {}
+    return True
+
+
+async def async_task_user_authenticate(user_id: int) -> dict | bool:
+    """
+    When is all OK, function returns the answer of Bool type. It's True. When we have Error or
+     not all is OK then we have an empty dictionary.
+    :param int user_id: Index of user.
+    :return:
+    """
+    client: type(Redis.client) = None
+    key = f"user:{user_id}:person"
+    user_json: TypeUser | None = None
+    try:
+        # Redis client with retries on custom errors
+        client = RedisOfPerson() if not client else client
+    except (ConnectionError, Exception) as error:
+        log.error(ValueError("%s: ERROR => %s" % (__name__, error.args.__getitem__(0))))
+        return {}
+    # Check tha ping for cache's db
+
+    try:
+        user_json = await client.async_basis_collection(user_id)
+        log.info(f"%s: Expected dict, got {user_json}" % (__name__))
+        if not user_json or not isinstance(user_json, dict):
+            log.error(
+                ValueError(f"%s: Expected dict, got {type(user_json)}" % (__name__))
+            )
+            return {}
+    except Exception as error:
+        log.error(ValueError("%s: ERROR => %s" % (__name__, error)))
+        return {}
+
+    try:
+        # UPDATE DATE FROM NON-RELATION DB
+        if user_json.get("login", False):
+            user_json.__setitem__("is_active", True)
+        if user_json.get("is_activated", False):
+            user_json.__setitem__("is_activated", True)
+        if user_json.get("is_verified", False):
+            user_json.__setitem__("is_verified", True)
+        user_json.__setitem__(
+            "date_joined", datetime.now().strftime("%Y-%m-%d %H:%M:%S.%u")
+        )
+        user_json.__setitem__(
+            "last_login", datetime.now().strftime("%Y-%m-%d %H:%M:%S.%u")
+        )
+
+        await client.async_set_cache_user(key, **user_json)
+        return True
+    except (JSONDecodeError, Exception) as error:
+        log.error(ValueError("%s: ERROR => %s" % (__name__, error)))
+        return {}
+    finally:
+        await client.aclose()
