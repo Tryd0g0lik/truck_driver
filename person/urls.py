ff69c1e1931ad75afe5a7d8a15a844eb0cb8d58f
Denis <work80@mail.ru>
2025-08-14 06:41:07 +0700

    [dev]: update static files and update - docker-compose

--- person/urls.py
+++ person/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from person.views import main_views
 from person.contribute.controler_activate import user_activate
-from project.urls_api import urlpatterns
+
 
 app_name = "person_app"
 urlpatterns = [
     path("", main_views, name="main_views"),
-    path("activate/<str:sign>/", user_activate, name="user_activate"),
+    path("register/", main_views, name="main_views"),
 ]
